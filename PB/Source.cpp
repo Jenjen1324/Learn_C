4c5c16ffa833a2022b88c908496344574a437c11
Jenjen1324 <northcodedotno@gmail.com>
2013-12-03 15:13:56 +0100

    Update

--- PB/Source.cpp
+++ PB/Source.cpp
@@ -1,13 +1,51 @@
 #include <stdio.h>
 #include <ctype.h>
 
+void A1(void);
+void A2(void);
 void A4(void);
+void A5(void);
 
 int main(void)
 {
-	A4();
+	A2();
+	fflush(stdin);
+	getc(stdin);
 }
 
+
+void A1(void)
+{
+	int a[10];
+	for (int i = 1; i <= 10; i++)
+	{
+		a[i - 1] = i;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", a[i]);
+	}
+}
+
+void A2(void)
+{
+	int a[255];
+
+	int i = 0,sum = 0;
+	do
+	{
+		printf("Geben Sie eine Zahl ein (0 für Abbruch und Berechnung): > ");
+		scanf("%d", &a[i]);
+		fflush(stdin);
+		if (a[i] == 0)
+			break;
+		sum += a[i];
+	} while (true);
+
+	printf("Summe: %d\n", sum);
+	printf("Mittelwert: %d", sum / i);
+}
 
 void A4(void)
 {
@@ -30,3 +68,10 @@ void A4(void)
 	fflush(stdin);
 	getc(stdin);
 }
+
+/// AUFGABE 5
+/*
+Es gibt Ja aus wenn der alle Buchstaben im string gleich sind wie der Letzte Buchstabe
+Wenn einer oder mehrere ungleich sind dann gibt es Nein aus
+
+*/
